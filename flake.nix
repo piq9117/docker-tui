5404e154fea3b4f865a1893e885db77aa91d80fb
piq9117 <piq9117@gmail.com>
2025-02-07 14:15:00 -0500

    got tie but docker api is old

--- flake.nix
+++ flake.nix
@@ -14,18 +14,49 @@
     {
       overlay = final: prev: {
         hsPkgs = prev.haskell.packages.ghc965.override {
-          overrides = hfinal: hprev: { };
+          overrides = hfinal: hprev: { 
+          openapi3 = prev.haskell.lib.dontCheck (hprev.openapi3.overrideAttrs (oldAttrs: {
+            src = builtins.fetchGit {
+              url = "git@github.com:alexbiehl/openapi3.git";
+              rev = "c2575dfbbf487c8019201ad1abe2d9b49fdc5685";
+              ref = "alex/aeson-2-support-for-extensions";
+            };
+          }));
+          };
         };
+
+        tie = final.hsPkgs.callCabal2nix "tie" (builtins.fetchGit {
+          url = "git@github.com:scarf-sh/tie.git";
+          rev = "e119add145f4e1cc13a870358f45606ced9ed06c";
+        }) {};
+
+        generate-api = final.writeScriptBin "generate-api" ''
+          GITROOT=$(git rev-parse --show-toplevel)
+          rm -rf "$GITROOT/api-lib"
+          ${final.tie}/bin/tie --output "$GITROOT/api-lib/docker " \
+            --module-name OpenAPI.Docker \
+            --package-name openapi-docker \
+            "$GITROOT/v1.47.yaml"
+        '';
+
         init-project = final.writeScriptBin "init-project" ''
           ${final.hsPkgs.cabal-install}/bin/cabal init --non-interactive
         '';
       };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+
+        in {
+          generate-api = pkgs.generate-api;
+        });
 
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
           libs = with pkgs; [
             zlib
+            tie
           ];
         in
         {
@@ -35,6 +66,7 @@
               hsPkgs.cabal-install
               hsPkgs.cabal-fmt
               hsPkgs.ghc
+              generate-api
               ormolu
               treefmt
               nixpkgs-fmt
--- src/Docker/Api.hs
+++ src/Docker/Api.hs
@@ -1,20 +1,9 @@
 module Docker.Api
-  ( DockerImage (..),
+  (
   )
 where
 
-import Data.Aeson (FromJSON (..), ToJSON (..))
-import Data.Time (UTCTime)
-
-data DockerImage = DockerImage
-  { containers :: Text,
-    createdAt :: UTCTime,
-    createdSince :: Text,
-    digest :: ByteString,
-    id :: Text,
-    repository :: Text,
-    shardSize :: Text,
-    tag :: Text,
-    uniqueSize :: Text,
-    virtualizeSize :: Text
+newtype DockerT input m a = DockerT
+  { runDocker :: input -> m a
   }
+
